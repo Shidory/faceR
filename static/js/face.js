61e407cd4ca80486b38db9b294dfc08db46b2fce
Simon MWEPU <simonmwepu@gmail.com>
2019-05-04 22:41:34 +0200

    [ADD]options

--- static/js/face.js
+++ static/js/face.js
@@ -1,4 +1,11 @@
 function detecter_face(){
 
     var PythonShell = require("python-shell");
+    var path = require("path");
+
+    var options ={
+        
+        pythonPath: 'usr/local/bin/python3',
+        scriptPath: path.join(__dirname, '../../controller')
+    }
 }
