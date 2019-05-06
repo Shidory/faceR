b70c02b3165b8ebac905e909421b09fe79983f9b
Simon MWEPU <simonmwepu@gmail.com>
2019-05-06 12:35:19 +0200

    [ADD]détection de la face

--- static/js/faces.js
+++ static/js/faces.js
@@ -28,5 +28,24 @@ function ajouter_face(){
 
   function detecter_face() {
 
+    document.getElementById("detect").value = "Hang on..."
+    var PythonShell = require("python-shell")
+    var path = require("path")
+  
+      var options = {
+      scriptPath : path.join(__dirname, '/controller/'),
+      pythonPath : '/usr/local/bin/python3'
+    }
+  
+    /*var face = new python("faces.py", options);
+  
+    face.end(function(err, code, message) {
+      document.getElementById("detect").value = "Detect faces";
+      })*/
+  
+      PythonShell.PythonShell.run('detecter_face.py', options, function (err, results) {
+        if (err) throw err;
+        document.getElementsById("detect").value = "Detect faces";
+      });
   }
   
